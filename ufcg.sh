65adee2e37755e64e1fd130f4dab44fe1e979cee
Wuttichai <34848296+wutto@users.noreply.github.com>
2024-10-31 10:55:28 +0700

    Update ufcg.sh

--- ufcg.sh
+++ ufcg.sh
@@ -1,5 +1,7 @@
 #[Step1] create ufcg profile
+cd ./example #go to example directory
 
+##Creating ufcg profile
 ufcg profile -t 16 -i Streptomyces_scabiei_WSLK1-1.fasta -o ./Streptomyces_scabiei_WSLK1-1-ufcgOut
 ufcg profile -t 16 -i Streptomyces_scabiei_WSLK1-9.fasta -o ./Streptomyces_scabiei_WSLK1-9-ufcgOut
 ufcg profile -t 16 -i Streptomyces_scabiei_WSLK1-11.fasta -o ./Streptomyces_scabiei_WSLK1-11-ufcgOut
